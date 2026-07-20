3bcfa91b171eae797a003247c126cfe60d3c2048
wakoliVotes <votesdw@yahoo.com>
2026-07-20 09:32:21 +0300

    2026-07-20 09:32:21

--- app/src/main/java/com/example/newsshorts/NewsApplication.kt
+++ app/src/main/java/com/example/newsshorts/NewsApplication.kt
@@ -1,6 +1,8 @@
 package com.example.newsshorts
 
 import android.app.Application
+import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NewsApplication : Application () {
--- app/src/main/java/com/example/newsshorts/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsshorts/ui/viewmodel/NewsViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.newsshorts.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 @HiltViewModel
 class NewsViewModel : ViewModel(){
